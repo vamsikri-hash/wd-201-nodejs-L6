db7617717384532667f2d1c6797b18f22df9c875
vamsi <bellamvamsikrishna3415@gmail.com>
2022-09-25 13:53:39 +0530

    Add all endpoints and fill get , markAsComplete

---
+++ migrations/20220925080734-create-todo.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY
+      },
+      completed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Todos');
+  }
+};
